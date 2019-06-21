351ebc16b306ca28af2c9b6ce9b6107aeb40832e
Rashard Pleasant <rashard.pleasant@gmail.com>
2019-06-21 00:30:25 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+def my_each [arry] 
+ #intiated a counter
+ counter = 0
+  
+   #created a while loop that itereates through all our elements in our array
+   while counter < arry.size do
+   #do something to the element in our array
+   yield (array[counter])
+   index += 1
 end
+
+
+  #return our array
+  array
+ end
