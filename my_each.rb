4d609a7ef204bf66ac8d3a85688b100a6d8190d0
Rashard Pleasant <rashard.pleasant@gmail.com>
2019-06-21 00:24:38 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,15 @@
 def my_each [arry] 
+ #intiated a counter
  counter = 0
- while counter < arry.size do
+  
+   #created a while loop that itereates through all our elements in our array
+   while counter < arry.size do
    #do something to the element in our array
+   yield (array[counter])
+   index += 1
 end
+
+
+  #return our array
+  array
+ end
